d44064d945b7c0ee38548127b5216403fb2b6959
TomZz <pythongolangkotlinrust@gmail.com>
2024-04-05 23:06:16 +0800

    🐞 fix: 修复时间参数缺失导致无法正常解析 所有的需要deserialize的Option可空类型都必须加上个#[serde(default)]确认结构体的字段默认为None，否则会报缺失字段的错误

--- src/dtos/back/notice.rs
+++ src/dtos/back/notice.rs
@@ -1,12 +1,16 @@
 #![allow(non_snake_case)]
+use chrono::NaiveDateTime;
 use serde::Deserialize;
+
+use crate::utils::serde::deserialize_option_naive_datetime;
 
 #[derive(Deserialize, Debug)]
 pub struct GetNoticeReq {
     pub page: Option<u32>,
     pub pageSize: Option<u32>,
-    // #[serde(deserialize_with = "deserialize_option_naive_datetime")]
-    // pub sendTime: Option<NaiveDateTime>,
+    #[serde(default)]
+    #[serde(deserialize_with = "deserialize_option_naive_datetime")]
+    pub sendTime: Option<NaiveDateTime>,
     pub status: Option<i32>,
     pub result: Option<String>,
 }
