84da0db483ef3887cb33a49f1479782bbcb82f4e
rust-kotlin <pythongolangkotlinrust@gmail.com>
2024-04-07 13:06:33 +0800

    🐞 fix: 修正状态错误码，完善自定义count中间件

--- src/middlewares/count.rs
+++ src/middlewares/count.rs
@@ -1,10 +1,11 @@
+#![allow(dead_code)]
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 use axum::{
     extract::{Request, State},
     middleware::Next,
-    response::{IntoResponse, Response},
+    response::Response,
 };
 use chrono::{Local, NaiveDate};
 use tokio::{fs::OpenOptions, io::AsyncWriteExt, sync::RwLock};
@@ -26,15 +27,11 @@ pub async fn count_middleware(
 
     if today != last_update {
         let count = state.count.load(Ordering::Relaxed);
-        let _res = update_count_file(count, &last_update).await;
-        // 不去处理这个错误
-        // if let Err(e) = _res {
-        //     return (
-        //         axum::http::StatusCode::INTERNAL_SERVER_ERROR,
-        //         format!("Failed to update count file: {}", e),
-        //     )
-        //         .into_response();
-        // }
+        let err_count = state.err_count.load(Ordering::Relaxed);
+        let _res = update_count_file(count, err_count, &last_update).await; // 不去处理这个错误
+        if _res.is_err() {
+            tracing::error!("更新计数文件失败");
+        }
         state.count.store(1, Ordering::Relaxed);
         let mut last_update = state.last_update.write().await;
         *last_update = today;
@@ -44,28 +41,32 @@ pub async fn count_middleware(
 
     let response = next.run(request).await;
 
-    // 如果Response为Error，增加错误计数
-    // if let Ok(body) = hyper::body::to_bytes(response.body()).await {
-    //     if let Ok(json) = serde_json::from_slice::<serde_json::Value>(&body) {
-    //         if let Some(code) = json.get("code").and_then(|v| v.as_u64()) {
-    //             if code != 200 {
-    //                 state.err_count.fetch_add(1, Ordering::Relaxed);
-    //             }
-    //         }
-    //     }
-    // }
+    // 如果Response为Error，增加错误计数，因为错误远比正确少，使用错误来计数减少性能消耗
+    if !response.status().is_success() {
+        state.err_count.fetch_add(1, Ordering::Relaxed);
+    }
 
     response
 }
 
-async fn update_count_file(count: usize, last_update: &NaiveDate) -> tokio::io::Result<()> {
+async fn update_count_file(
+    count: usize,
+    err_count: usize,
+    last_update: &NaiveDate,
+) -> tokio::io::Result<()> {
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
         .open("count.txt")
         .await?;
-    let data = format!("{} {}\n", last_update, count);
+    let data = format!(
+        "{} total: {}, successed: {}, rate: {}%\n",
+        last_update,
+        count,
+        count - err_count,
+        100 - err_count * 100 / count
+    );
 
     file.write_all(data.as_bytes()).await?;
 
