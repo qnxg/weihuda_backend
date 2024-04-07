4e683d475e9f68cbeda6674ecf1a97cb5d86dea0
TomZz <pythongolangkotlinrust@gmail.com>
2024-04-07 13:06:33 +0800

    🐞 fix: 修正状态错误码，完善自定义count中间件

--- src/app_error.rs
+++ src/app_error.rs
@@ -44,29 +44,32 @@ impl IntoResponse for AppError {
         match self {
             AppError::AnyHow(e) => {
                 tracing::error!("服务器内部错误 {}", e);
-                (StatusCode::OK, error_json(500, &format!("内部错误: {}", e))).into_response()
+                (StatusCode::INTERNAL_SERVER_ERROR, error_json(500, &format!("内部错误: {}", e)))
+                    .into_response()
                 // 展示给前端方便定位错误原因
             }
             AppError::QueryError(e) => {
                 tracing::error!("Query参数错误 {}", e);
-                (StatusCode::OK, error_json(400, "参数解析错误")).into_response()
+                (StatusCode::BAD_REQUEST, error_json(400, "参数解析错误")).into_response()
                 // 错误信息默认只提示消耗stream流过程中第一个缺失的字段
             }
             AppError::JsonError(e) => {
                 tracing::error!("Json请求体参数错误 {}", e);
-                (StatusCode::OK, error_json(400, "Body中Json参数解析错误")).into_response()
+                (StatusCode::BAD_REQUEST, error_json(400, "Body中Json参数解析错误"))
+                    .into_response()
             }
             AppError::SqlxError(e) => {
                 tracing::error!("数据库SQL语句执行错误 {}", e);
-                (StatusCode::OK, error_json(500, "数据库内部错误")).into_response()
+                (StatusCode::INTERNAL_SERVER_ERROR, error_json(500, "数据库内部错误"))
+                    .into_response()
             }
             AppError::JwtError(e) => {
                 tracing::error!("JWT编解码错误 {}", e);
-                (StatusCode::OK, error_json(401, "身份验证错误")).into_response()
+                (StatusCode::UNAUTHORIZED, error_json(401, "身份验证错误")).into_response()
             }
             AppError::ParseError(e) => {
                 tracing::error!("时间解析错误 {}", e);
-                (StatusCode::OK, error_json(400, "时间解析错误")).into_response()
+                (StatusCode::BAD_REQUEST, error_json(400, "时间解析错误")).into_response()
             }
         }
     }
