c9b6d58227535f1d3c770ac1753ec3874c9e3fe1
TomZz <pythongolangkotlinrust@gmail.com>
2024-04-20 01:25:26 +0800

    🐞 fix: 修复8点之前每日阅读上限失效

--- src/handlers/back/record.rs
+++ src/handlers/back/record.rs
@@ -463,8 +463,8 @@ pub async fn get_webview_read_handler(
 
     // 查询周期内的积分记录
     let now = chrono::Local::now();
-    let create_time_greater_than = now - chrono::Duration::days(record_rule.cycle as i64 - 1);
-    let create_time_greater_than_naive_datetime = create_time_greater_than.date_naive().and_hms_opt(0, 0, 0).unwrap();  // 设置时分秒为0
+    let create_time_greater_than = now - chrono::Duration::days(record_rule.cycle as i64);  // 不用再减1，因为Utc时间需要是前一天的16点对应北京时间24点，所以其实要多减一天，括号内为+1天，消掉-1天
+    let create_time_greater_than_naive_datetime = create_time_greater_than.date_naive().and_hms_opt(16, 0, 0).unwrap();  // 设置时分秒为0
 
     let count = sqlx::query!(
         r#"
@@ -589,8 +589,8 @@ mod tests {
     async fn test() {
         // 查询周期内的积分记录
         let now = chrono::Local::now();
-        let create_time_greater_than = now - chrono::Duration::days(1 - 1);
-        let create_time_greater_than = create_time_greater_than.date_naive().and_hms_opt(0, 0, 0).unwrap();
+        let create_time_greater_than = now - chrono::Duration::days(1 + 1 - 1);
+        let create_time_greater_than = create_time_greater_than.date_naive().and_hms_opt(16, 0, 0).unwrap();
         let create_time_greater_than_str = create_time_greater_than.format("%Y-%m-%d %H:%M:%S").to_string();
         println!("{}", create_time_greater_than);
         println!("{}", create_time_greater_than_str);
