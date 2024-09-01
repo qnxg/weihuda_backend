331d07d7e54b015e447bf31e68fc30bd0370ad1c
TomZz <pythongolangkotlinrust@gmail.com>
2024-09-01 23:04:40 +0800

    代码改动：完善必修成绩逻辑处理

--- src/handlers/spider/hdjw.rs
+++ src/handlers/spider/hdjw.rs
@@ -171,6 +171,9 @@ pub async fn get_must_grade_handler(
             credits += item.xf;
         }
     }
+    if { credits == 0.0 } {
+        return Err("无数据".into());
+    }
     let weighted_avg = scores / credits;
     // 转换成String，只保留两位小数
     let weighted_avg = format!("{:.2}", weighted_avg);
