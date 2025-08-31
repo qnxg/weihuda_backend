67e0dc376517d0abf92c3091703b4367ecbbc782
caiwen <3102733279@qq.com>
2025-08-31 14:28:58 +0800

    更新空教室查询接口

--- src/entities/spider/empty_room.rs
+++ src/entities/spider/empty_room.rs
@@ -1,29 +1,5 @@
 #![allow(non_snake_case)]
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Debug)]
-pub struct SpiderEmptyRoom {
-    pub items: Vec<SpiderEmptyRoomItem>,
-    pub rowCount: u32,
-}
-
-/// 只解析需要的字段，就不全部解析了，其他字段写上也没用
-#[derive(Deserialize, Debug)]
-pub struct SpiderEmptyRoomItem {
-    pub classroomtypename: String,
-    pub js_name: String,
-    pub kszw: u32,
-    pub yxzw: u32,
-    // pub bh: String,
-    // pub classroomtypecode: String,
-    // pub id: String,
-    // pub jczy002id: String,
-    // pub jczy008id: String,
-    // pub jxl: String,
-    // pub rnrs: u32,
-    // pub rownum_: u32,
-    // pub xqname: String,
-}
+use serde::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct EmptyRoomRes {
