aea4d306b27119e6e738f59e3322819a829c91a6
caiwen <3102733279@qq.com>
2025-09-01 10:04:21 +0800

    修复调休

--- src/entities/back/flex_time.rs
+++ src/entities/back/flex_time.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 /// 将会将 from 的课程全部转移到 to 上去，且 to 的课程全部毙掉
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FlexTime {
-    pub from: FlexDay,
+    pub from: Option<FlexDay>,
     pub to: FlexDay,
     pub desc: String, // 描述，将会返回给前端用作展示
     pub time: XnXq,   // 学年学期
