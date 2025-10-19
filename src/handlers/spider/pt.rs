732d15ec2e8a6d0330787d941cdf95a2670337c4
skr2005 <62237294+skr2005@users.noreply.github.com>
2025-10-19 21:22:16 +0800

    feat(fitness): 在后端处理性别信息 (#26)
    close #20
    
    还是用的`spider_data`，重构放到以后（
    
    还把体测的三个请求并行了，说不定比以前要快一些
    
    Co-authored-by: skr2005 <62237294+skr2005@users.noreply.github.com>
    Reviewed-on: http://code.qnxg.cn/qnxg/back_2024/pulls/26

--- src/handlers/spider/pt.rs
+++ src/handlers/spider/pt.rs
@@ -2,7 +2,10 @@ use axum::Extension;
 
 use crate::{
     app_result::AppResult,
-    dtos::spider::pt::{GetCardHistoryReq, GetFitnessReq},
+    dtos::spider::{
+        pt::{GetCardHistoryReq, GetFitnessReq},
+        xgxt::PersonInfo,
+    },
     entities::spider::{
         card::{
             CardHistoryRes, CardHistoryResItem, CardInfoRes,
@@ -102,151 +105,178 @@ pub async fn get_fitness_handler(
     Extension(token): Extension<String>,
 ) -> AppResult {
     let stu_id = parse_stu_id(&token)?;
-    let params = [("stuid", stu_id), ("xn", req.xn)];
-    let data: SpiderFitness = spider("/gymos/grade", &params).await?;
-    let raw: SpiderFitnessRaw =
-        spider_data("/gymos/raw_grade", &params).await?;
 
-    let mut res = FitnessRes {
+    let param_stuid_xn = &[("stuid", &stu_id), ("xn", &req.xn)];
+    let param_stuid = &[("stuid", &stu_id), ("xn", &req.xn)];
+
+    let (grade, raw_grade, person_info) = tokio::try_join!(
+        async {
+            spider::<_, SpiderFitness>("/gymos/grade", param_stuid_xn)
+                .await
+                .map_err(|e| e.into())
+        },
+        spider_data::<_, SpiderFitnessRaw>(
+            "/gymos/raw_grade",
+            param_stuid_xn,
+        ),
+        spider_data::<_, PersonInfo>(
+            "/xgxt/person_info",
+            param_stuid,
+        )
+    )?;
+
+    let res = FitnessRes {
         student: FitnessResStudent {
-            name: raw.student_name,
-            number: raw.student_num,
+            name: raw_grade.student_name,
+            number: raw_grade.student_num,
         },
         total: FitnessResTotal {
-            score: raw.total_score,
-            grade: raw.total_grade,
+            score: raw_grade.total_score,
+            grade: raw_grade.total_grade,
         },
         report: FitnessResReport {
-            desc: raw.report_desc,
-            status: data
+            desc: raw_grade.report_desc,
+            status: grade
                 .report_status
-                .unwrap_or(raw.status.to_string()),
-            _type: raw.report_type.to_string(),
+                .unwrap_or(raw_grade.status.to_string()),
+            _type: raw_grade.report_type.to_string(),
         },
         eye: FitnessResEye {
             eyesight_right: format!(
                 "{} {}",
-                raw.eyesight_right, raw.eyesight_right_detail
+                raw_grade.eyesight_right,
+                raw_grade.eyesight_right_detail
             ),
             eyesight_left: format!(
                 "{} {}",
-                raw.eyesight_left, raw.eyesight_left_detail
+                raw_grade.eyesight_left,
+                raw_grade.eyesight_left_detail
             ),
             eye_mirror_right: format!(
                 "{} {}",
-                raw.eye_mirror_right, raw.eye_mirror_right_detail
+                raw_grade.eye_mirror_right,
+                raw_grade.eye_mirror_right_detail
             ),
             eye_mirror_left: format!(
                 "{} {}",
-                raw.eye_mirror_left, raw.eye_mirror_left_detail
+                raw_grade.eye_mirror_left,
+                raw_grade.eye_mirror_left_detail
             ),
             eye_ametropia_right: format!(
                 "{} {}",
-                raw.eye_ametropia_right,
-                raw.eye_ametropia_right_detail
+                raw_grade.eye_ametropia_right,
+                raw_grade.eye_ametropia_right_detail
             ),
             eye_ametropia_left: format!(
                 "{} {}",
-                raw.eye_ametropia_left, raw.eye_ametropia_left_detail
+                raw_grade.eye_ametropia_left,
+                raw_grade.eye_ametropia_left_detail
             ),
         },
-        items: Vec::new(), // To be filled
+        items: vec![
+            FitnessResItem {
+                name: "50m".to_string(),
+                class: grade.data.short_run_class.unwrap_or(
+                    get_class_color(&raw_grade.short_run_grade),
+                ),
+                score: grade
+                    .data
+                    .short_run_score
+                    .unwrap_or(raw_grade.short_run + "秒"),
+                rank: raw_grade.short_run_grade,
+                grade: raw_grade.short_run_score,
+            },
+            FitnessResItem {
+                name: "BMI".to_string(),
+                class: grade
+                    .data
+                    .bmi_class
+                    .unwrap_or(get_class_color(&raw_grade.bmi_grade)),
+                score: grade.data.bmi_score.unwrap_or(format!(
+                    "{}厘米/{}千克",
+                    raw_grade.height, raw_grade.weight
+                )),
+                rank: raw_grade.bmi_grade,
+                grade: raw_grade.bmi_score,
+            },
+            FitnessResItem {
+                name: "跳远".to_string(),
+                class: grade.data.jump_class.unwrap_or(
+                    get_class_color(&raw_grade.jump_grade),
+                ),
+                score: grade
+                    .data
+                    .jump_score
+                    .unwrap_or(raw_grade.jump + "厘米"),
+                rank: raw_grade.jump_grade,
+                grade: raw_grade.jump_score,
+            },
+            FitnessResItem {
+                name: if person_info.gender == "男" {
+                    "引体向上"
+                } else {
+                    "仰卧起坐"
+                }
+                .to_string(),
+                class: grade.data.pull_and_sit_class.unwrap_or(
+                    get_class_color(&raw_grade.pull_and_sit_grade),
+                ),
+                score: grade
+                    .data
+                    .pull_and_sit_score
+                    .unwrap_or(raw_grade.pull_and_sit.to_string()),
+                rank: raw_grade.pull_and_sit_grade,
+                grade: raw_grade.pull_and_sit_score
+                    + raw_grade.extra_score_pull_or_sit_up,
+            },
+            FitnessResItem {
+                name: "长跑".to_string(),
+                class: grade
+                    .data
+                    .run_class
+                    .unwrap_or(get_class_color(&raw_grade.run_grade)),
+                score: grade.data.run_score.unwrap_or({
+                    let total_seconds: u32 =
+                        raw_grade.run.parse().unwrap_or(0);
+                    let minutes = total_seconds / 60;
+                    let seconds = total_seconds - minutes * 60;
+                    if seconds != 0 {
+                        format!("{}'{}\"", minutes, seconds)
+                    } else {
+                        format!("{}'", minutes)
+                    }
+                }),
+                rank: raw_grade.run_grade,
+                grade: raw_grade.run_score
+                    + raw_grade.extra_score_run,
+            },
+            FitnessResItem {
+                name: "坐位体前屈".to_string(),
+                class: grade.data.sit_and_reach_class.unwrap_or(
+                    get_class_color(&raw_grade.sit_and_reach_grade),
+                ),
+                score: grade
+                    .data
+                    .sit_and_reach_score
+                    .unwrap_or(raw_grade.sit_and_reach + "厘米"),
+                rank: raw_grade.sit_and_reach_grade,
+                grade: raw_grade.sit_and_reach_score,
+            },
+            FitnessResItem {
+                name: "肺活量".to_string(),
+                class: grade
+                    .data
+                    .vc_class
+                    .unwrap_or(get_class_color(&raw_grade.vc_grade)),
+                score: grade
+                    .data
+                    .vc_score
+                    .unwrap_or(raw_grade.vc.to_string() + "毫升"),
+                rank: raw_grade.vc_grade,
+                grade: raw_grade.vc_score,
+            },
+        ],
     };
-    res.items.push(FitnessResItem {
-        name: "50m".to_string(),
-        class: data
-            .data
-            .short_run_class
-            .unwrap_or(get_class_color(&raw.short_run_grade)),
-        score: data
-            .data
-            .short_run_score
-            .unwrap_or(raw.short_run + "秒"),
-        rank: raw.short_run_grade,
-        grade: raw.short_run_score,
-    });
-    res.items.push(FitnessResItem {
-        name: "BMI".to_string(),
-        class: data
-            .data
-            .bmi_class
-            .unwrap_or(get_class_color(&raw.bmi_grade)),
-        score: data.data.bmi_score.unwrap_or(format!(
-            "{}厘米/{}千克",
-            raw.height, raw.weight
-        )),
-        rank: raw.bmi_grade,
-        grade: raw.bmi_score,
-    });
-    res.items.push(FitnessResItem {
-        name: "跳远".to_string(),
-        class: data
-            .data
-            .jump_class
-            .unwrap_or(get_class_color(&raw.jump_grade)),
-        score: data.data.jump_score.unwrap_or(raw.jump + "厘米"),
-        rank: raw.jump_grade,
-        grade: raw.jump_score,
-    });
-    res.items.push(FitnessResItem {
-        name: "引体向上/仰卧起坐".to_string(),
-        class: data
-            .data
-            .pull_and_sit_class
-            .unwrap_or(get_class_color(&raw.pull_and_sit_grade)),
-        score: data
-            .data
-            .pull_and_sit_score
-            .unwrap_or(raw.pull_and_sit.to_string()),
-        rank: raw.pull_and_sit_grade,
-        grade: raw.pull_and_sit_score
-            + raw.extra_score_pull_or_sit_up,
-    });
-    res.items.push(FitnessResItem {
-        name: "长跑".to_string(),
-        class: data
-            .data
-            .run_class
-            .unwrap_or(get_class_color(&raw.run_grade)),
-        score: data.data.run_score.unwrap_or({
-            let total_seconds: u32 = raw.run.parse().unwrap_or(0);
-            let minutes = total_seconds / 60;
-            let seconds = total_seconds - minutes * 60;
-            if seconds != 0 {
-                format!("{}'{}\"", minutes, seconds)
-            } else {
-                format!("{}'", minutes)
-            }
-        }),
-        rank: raw.run_grade,
-        grade: raw.run_score + raw.extra_score_run,
-    });
-    res.items.push(FitnessResItem {
-        name: "坐位体前屈".to_string(),
-        class: data
-            .data
-            .sit_and_reach_class
-            .unwrap_or(get_class_color(&raw.sit_and_reach_grade)),
-        score: data
-            .data
-            .sit_and_reach_score
-            .unwrap_or(raw.sit_and_reach + "厘米"),
-        rank: raw.sit_and_reach_grade,
-        grade: raw.sit_and_reach_score,
-    });
-    res.items.push(FitnessResItem {
-        name: "肺活量".to_string(),
-        class: data
-            .data
-            .vc_class
-            .unwrap_or(get_class_color(&raw.vc_grade)),
-        score: data
-            .data
-            .vc_score
-            .unwrap_or(raw.vc.to_string() + "毫升"),
-        rank: raw.vc_grade,
-        grade: raw.vc_score,
-    });
     Ok(res.into())
 }
 
