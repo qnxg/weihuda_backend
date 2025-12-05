7b6dc8610d46800eb37223302b2c305564b9af68
caiwen <3102733279@qq.com>
2025-12-05 19:02:52 +0800

    feat: 课程成绩详情 (#40)
    Reviewed-on: http://code.qnxg.cn/qnxg/back_2024/pulls/40
    Co-authored-by: caiwen <3102733279@qq.com>
    Co-committed-by: caiwen <3102733279@qq.com>

--- src/routers/grade_rank.rs
+++ src/routers/grade_rank.rs
@@ -13,7 +13,11 @@ use crate::{
 
 pub fn routers() -> Router {
     Router::with_path("hdjw")
-        .push(Router::with_path("grade").get(get_grade))
+        .push(
+            Router::with_path("grade").get(get_grade).push(
+                Router::with_path("detail").get(get_grade_detail),
+            ),
+        )
         .push(Router::with_path("grade-rank").get(get_rank_from_hdjw))
         .push(
             Router::with_path("grade-rank-from-ca")
@@ -79,5 +83,19 @@ async fn get_rank_from_hdjw(req: &mut Request) -> RouterResult {
 async fn get_rank_from_ca(req: &mut Request) -> RouterResult {
     let (_, stu_id) = utils::jwt::auth(req)?;
     let res = service::grade_rank::get_rank_from_ca(&stu_id).await?;
+    Ok(res.into())
+}
+
+#[derive(Deserialize, Debug)]
+struct GetGradeDetailReq {
+    pub jx0404id: String,
+}
+#[handler]
+async fn get_grade_detail(req: &mut Request) -> RouterResult {
+    let (_, stu_id) = utils::jwt::auth(req)?;
+    let GetGradeDetailReq { jx0404id } = req.parse_queries()?;
+    let res =
+        service::grade_rank::get_grade_detail(&stu_id, &jx0404id)
+            .await?;
     Ok(res.into())
 }
--- src/service/grade_rank.rs
+++ src/service/grade_rank.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     infra::{self},
     result::AppResult,
@@ -12,14 +14,15 @@ pub use infra::spider::hdjw::RankRange as HdjwRankRange;
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct GradeInfo {
-    pub course_id: String,    // 课程代码
-    pub course_name: String,  // 课程名称
-    pub credit: f32,          // 学分
-    pub course_type1: String, // 课程性质1（必修还是选修）
-    pub course_type2: String, // 课程性质2（通识必修/专业核心等）
-    pub gpa: f32,             // 绩点
-    pub score: u8,            // 成绩
+    pub course_id: String,        // 课程代码
+    pub course_name: String,      // 课程名称
+    pub credit: f32,              // 学分
+    pub course_type1: String,     // 课程性质1（必修还是选修）
+    pub course_type2: String,     // 课程性质2（通识必修/专业核心等）
+    pub gpa: f32,                 // 绩点
+    pub score: u8,                // 成绩
     pub tags: Vec<String>, // 其他标签，如缓考还是什么（参考 SpiderGradeInfo 的 cjbs 说明），或者辅修等
+    pub jx0404id: Option<String>, // 用于获取成绩详情
 }
 pub async fn get_grade(
     xn: u32,
@@ -46,6 +49,7 @@ pub async fn get_grade(
             gpa: item.jd,
             score: item.zcj,
             tags,
+            jx0404id: item.jx0404id,
         };
         res.push(tmp);
     }
@@ -53,6 +57,7 @@ pub async fn get_grade(
 }
 
 pub use infra::spider::hdjw::get_rank as get_rank_from_hdjw;
+use serde_json::Value;
 
 // 这样做为了做到 ca::Rank 的效果
 pub mod ca {
@@ -123,4 +128,118 @@ pub async fn get_rank_from_ca(stu_id: &str) -> AppResult<ca::Rank> {
         core_weighted_rank,
     };
     Ok(res)
+}
+
+#[derive(Serialize, Debug)]
+pub struct GradeDetailItem {
+    pub name: String,
+    pub score: String,
+    pub percentage: String,
+}
+pub async fn get_grade_detail(
+    stu_id: &str,
+    jx0404id: &str,
+) -> AppResult<Vec<GradeDetailItem>> {
+    let spider_res =
+        infra::spider::hdjw::get_grade_detail(stu_id, jx0404id)
+            .await?;
+    let regex = RegexBuilder::new(
+        r"let\sarr\s=\s(.*);.*window.initQzTable\(\{.*cols:\s\[(.*)\].*\}\);",
+    )
+    .dot_matches_new_line(true)
+    .build()
+    .expect("构建正则表达式失败");
+    let caps = regex
+        .captures(&spider_res)
+        .ok_or(anyhow!("解析成绩详情数据失败"))?
+        .iter()
+        .map(|c| {
+            c.map(|v| v.as_str().to_string())
+                .ok_or(anyhow!("解析成绩详情数据失败: 字段为空"))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    let [_, data, map] = caps
+        .try_into()
+        .map_err(|_| anyhow!("解析成绩详情数据失败: 匹配数量错误"))?;
+    let data = serde_json::from_str::<Vec<Value>>(&data).ok();
+    let data = data.as_ref()
+        .and_then(|v| v.first())
+        .and_then(|v| v.as_object()).map(|v|
+            v.iter().map(|(key, value)|{
+                value.as_str().map(|s| s.to_string()).or(
+                        value
+                            .as_number().map(|num| num.to_string()),
+                    ).ok_or(anyhow!("解析成绩详情数据失败: 字段不是字符串或数字"))
+                    .map(|ok_value| (key, ok_value))
+            }
+            ).collect::<Result<HashMap<_, _>,_>>())
+        .ok_or(anyhow!("解析成绩详情数据失败: data"))??;
+    // map 是 js obj 格式，不是标准 json，我们需要进行一些处理
+    let map = map
+        .replace("//表头", "")
+        .replace("'", "\"")
+        .replace("field", "\"field\"")
+        .replace("title", "\"title\"")
+        .replace("type", "\"type\"");
+    let map = serde_json::from_str::<Value>(map.as_str()).ok();
+    let map = map
+        .as_ref()
+        .and_then(|v| v.as_array())
+        .map(|v| {
+            v.iter()
+                .filter(|item| {
+                    item.get("field")
+                        .and_then(|f| f.as_str())
+                        .is_some()
+                })
+                .map(|item| {
+                    let key =
+                        item.get("field").and_then(|f| f.as_str());
+                    item.get("title")
+                        .and_then(|f| f.as_str())
+                        .and_then(|value| key.map(|key| (key, value)))
+                        .ok_or(anyhow!("解析成绩详情数据失败: map"))
+                })
+                .collect::<Result<HashMap<_, _>, _>>()
+        })
+        .ok_or(anyhow!("解析成绩详情数据失败: map"))??;
+    let res = data
+        .iter()
+        .filter(|(k, _)| k.ends_with("bl"))
+        .map(|(k, v)| {
+            let score = data
+                .get(&k.trim_end_matches("bl").to_string())
+                .ok_or(anyhow!(
+                    "解析成绩详情数据失败: data 缺失 {}",
+                    k.trim_end_matches("bl")
+                ))?;
+            let name = map.get(k.trim_end_matches("bl")).ok_or(
+                anyhow!("解析成绩详情数据失败: map 缺失 {}", k),
+            )?;
+            let percentage = v;
+            Ok::<_, anyhow::Error>(GradeDetailItem {
+                score: score.to_string(),
+                name: name.to_string(),
+                percentage: percentage.to_string(),
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .filter(|item| item.percentage != "0%")
+        .collect::<Vec<_>>();
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const STUID: &str = "";
+
+    #[tokio::test]
+    async fn test_get_grade_detail() {
+        let res =
+            get_grade_detail(STUID, "TB001TY24I-373").await.unwrap();
+        println!("{:#?}", res);
+    }
 }
